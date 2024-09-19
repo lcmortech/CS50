6f41d10221633c530396976a3bedc35085ea00d5
Ren <rg326@live.com>
2024-09-19 09:17:38 -0400

    added text to read me, added file hello.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CS50
 Notes for CS50 lectures
+# This repo contains lecture notes and assignments on:
+- CS50P
+- CS50W
+- CS50X
+- CS50AI
---
+++ lec0-funcs/1_hello.py
@@ -0,0 +1,8 @@
+# Ask user for their name
+name = input("What is your name?").strip().title()
+
+# Split user's name into first name and last name
+first, last = name.split("")
+
+# Say hello to user
+print(f"Hello, {name}")
