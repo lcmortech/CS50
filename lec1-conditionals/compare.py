577a38832e6cd89fc5bb5386df5bb46a85f8d350
Ren <rg326@live.com>
2024-09-21 23:11:57 -0400

    added notes on else conditional to compare.py

--- lec1-conditionals/compare.py
+++ lec1-conditionals/compare.py
@@ -6,5 +6,7 @@ if x < y:
 # adding elif(else if) helps make the code more readable and logical
 elif x > y:
     print("x is greater than y")
-elif x == y:
+elif x == y: # can be replaced with else
     print("x is equal to y")
+    
+# else is not necessary here because all conditions have already been satisfied
