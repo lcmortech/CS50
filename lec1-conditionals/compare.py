1715b1199addcbb59a61bfbaacb73b268a2abdbc
Ren <rg326@live.com>
2024-09-21 23:11:57 -0400

    same as prev and added notes for elif to compare.py

--- lec1-conditionals/compare.py
+++ lec1-conditionals/compare.py
@@ -3,6 +3,7 @@ y=int(input("What is Y?" ))
 
 if x < y:
     print("x is less than y")
+# adding elif(else if) helps make the code more readable and logical
 elif x > y:
     print("x is greater than y")
 elif x == y:
