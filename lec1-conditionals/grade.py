6536cd192f25b1156fd966795f66703880f85acd
Ren <rg326@live.com>
2024-09-26 21:27:27 -0400

    simplified example code for grade.py

--- lec1-conditionals/grade.py
+++ lec1-conditionals/grade.py
@@ -1,5 +1,6 @@
 score = int(input("Score: "))
 
+'''
 if 90 <= score <= 100:
     print("Grade A")
 elif 80 <= score <= 90:
@@ -7,6 +8,19 @@ elif 80 <= score <= 90:
 elif 70 <= score <= 80:
     print("Grade C")
 elif 60 <= score <= 70:
+    print("Grade D")
+else:
+    print("Grade F")
+'''
+
+# elif makes the conditional mutually exclusive
+if score >= 100:
+    print("Grade A")
+elif score >= 90:
+    print("Grade B")
+elif score >= 80:
+    print("Grade C")
+elif score >= 70:
     print("Grade D")
 else:
     print("Grade F")
