6fbab4128b4775b17dd7b0b2f92ae352c96391ed
Ren <rg326@live.com>
2024-09-28 16:45:27 -0400

    added for loop to 8_cat.py

--- lec2-loops/8_cat.py
+++ lec2-loops/8_cat.py
@@ -17,9 +17,39 @@ while i > 0:
     print("meow")
     i -= 1 
 '''
-
+'''
 i = 1
 while i <= 3:
     print("meow")
     i += 1
+    '''
 # decrement/increment necessary to prevent an infinite loop
+
+# introducing the for loop and lists
+# the for loop simplifies alot of the processes of a while lop
+
+# more pythonic to add an underscore than i as a default iterator
+'''
+for _ in range(4):
+    print("meow")
+'''
+
+# more pythonic overall, but a bit unreadable
+print("meow \n" * 3,end="")
+
+# when you want to get user input that matches a certain expectation you have, you use while True:
+'''
+n = int(input("What's n? "))
+if n < 0:
+    n = int(input)... # gets confusing
+'''
+
+# introducing continue and break statements
+while True:
+    n = int(input("What's n? "))
+    if n < 0:
+        continue
+    else:
+        break
+
+# more useable
