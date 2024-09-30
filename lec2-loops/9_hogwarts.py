bf0c3d99e4e53be9025203b5a1f0bbfa56aa94c3
Lauren <rg326@live.com>
2024-09-29 23:46:39 -0400

    added ex code to 9_hogwarts.py

---
+++ lec2-loops/9_hogwarts.py
@@ -0,0 +1,11 @@
+# using lists with for loops
+
+students = ["Hermoine", "Harry", "Ron"]
+
+print(students[0])
+print(students[1])
+print(students[2])
+print(students[3])
+
+for student in students:
+    print(students[student])
