75ddcb8a23a278720056c2e303d9453500bf4032
Ren <rg326@live.com>
2024-09-30 23:07:49 -0400

    added example dict to 9_hogwarts.py

--- lec2-loops/9_hogwarts.py
+++ lec2-loops/9_hogwarts.py
@@ -14,11 +14,39 @@ for student in students:
 students = ["Hermoine", "Harry", "Ron", "Draco"]
 houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
 '''
+'''
 students = {
 "Hermoine":"Gryffindor", 
 "Harry": "Gryffindor"},
 "Ron": "Gryffindor",
 "Draco": "Slytherin"
 }
+'''
+'''
 
 print(students["Hermoine"])
+print(students["Harry"])
+print(students["Ron"])
+print(students["Draco"])
+'''
+# when you use a for loop over a dict, it only returns the kes by default:
+# print(student) 
+'''
+for student in students:
+    print(students[student])
+
+
+for student in students:
+    print(student, students[student])
+'''
+
+# there are three keys in the following dictionary:
+students = [
+    {"name": "Hermoine", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"}, 
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
+
+for student in students:
+    print(student["name"], student["house"], student["patronus", sep=", "])
