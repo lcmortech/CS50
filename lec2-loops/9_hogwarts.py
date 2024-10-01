70effa2a59bb57e7e6d35fe288f6936d0378382b
Ren <rg326@live.com>
2024-09-30 23:07:49 -0400

    introducing dictionary data type in 9_hogwarts.py

--- lec2-loops/9_hogwarts.py
+++ lec2-loops/9_hogwarts.py
@@ -1,5 +1,5 @@
 # using lists with for loops
-
+'''
 students = ["Hermoine", "Harry", "Ron"]
 
 print(students[0])
@@ -9,3 +9,16 @@ print(students[3])
 
 for student in students:
     print(students[student])
+'''
+'''
+students = ["Hermoine", "Harry", "Ron", "Draco"]
+houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+'''
+students = {
+"Hermoine":"Gryffindor", 
+"Harry": "Gryffindor"},
+"Ron": "Gryffindor",
+"Draco": "Slytherin"
+}
+
+print(students["Hermoine"])
