96e43ab82827d36d46cf5d93cc7325d9792911a8
Ren <rg326@live.com>
2024-10-01 23:47:41 -0400

    added simple program to print columns in 10_mario.py

---
+++ lec2-loops/10_mario.py
@@ -0,0 +1,13 @@
+# using what we learned to recreate the blocks in mario
+'''
+for _ in range(3):
+    print("#")
+'''
+
+def main():
+    print_column(3)
+
+def print_column():
+    print("#/n" * height,end="")
+
+main()
