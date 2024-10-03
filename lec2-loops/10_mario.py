3916f9af68088b0885e27ab0ce8dae675bd7eb3a
Ren <rg326@live.com>
2024-10-02 23:13:15 -0400

    printed the question marks on the mario blocks in 10_mario.py

--- lec2-loops/10_mario.py
+++ lec2-loops/10_mario.py
@@ -4,10 +4,21 @@ for _ in range(3):
     print("#")
 '''
 
+'''
 def main():
     print_column(3)
 
 def print_column():
-    print("#/n" * height,end="")
+    print("#/n" * height, end="")
+
+main()
+'''
+
+def main():
+    print_row(4)
+
+def print_row():
+    print("?" * width)
+
 
 main()
