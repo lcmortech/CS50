e110a1df325fc502e646bbba04db5b3ec40b9fbf
Ren <rg326@live.com>
2024-10-02 23:13:15 -0400

    added code to print a block using nested loops in 10_mario.py

--- lec2-loops/10_mario.py
+++ lec2-loops/10_mario.py
@@ -13,7 +13,7 @@ def print_column():
 
 main()
 '''
-
+'''
 def main():
     print_row(4)
 
@@ -22,3 +22,43 @@ def print_row():
 
 
 main()
+'''
+'''
+def main():
+    print_square(3)
+
+# outer loop represents each row in square
+def print_square(size):
+
+    # for each brick in row:
+    for i in range(size):
+        for j in range(size):
+
+            # print each brick
+            print("g",end="")
+
+        # prints a new line at the end of each row by default, not at the end of each brick
+        print()
+'''
+
+# ALT =======
+# similar to earlier print_row() function
+'''
+def print_square(size):
+	for i in range(size):
+		print("g" * size)
+'''
+# ===========
+
+def main():
+    print_row(size)
+
+# outer loop represents each row in square
+def print_square(size):
+
+    # for each brick in row:
+    for i in range(size):
+        print_row(size)
+
+def print_row(width):
+	print("g"* width)
