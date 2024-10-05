cdaa85267f3c36996521d8e1320c45ef89e3d402
Ren <rg326@live.com>
2024-10-04 23:48:29 -0400

    introduced try/except block to 12_number.py

--- lec3-exceptions/12_number.py
+++ lec3-exceptions/12_number.py
@@ -1,1 +1,8 @@
-x = int(input("What's x? "))
+try:
+    x = int(input("What's x? "))
+    print(f"x is {x}")
+
+except:
+    print("Please input a number")
+
+# the user might input a string (value error)
