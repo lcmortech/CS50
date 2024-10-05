2834a7ea11ad5efec5a14e19d48930b5b28263d1
Ren <rg326@live.com>
2024-10-04 23:48:29 -0400

    included valueerror keyword to except block in 12_number.py

--- lec3-exceptions/12_number.py
+++ lec3-exceptions/12_number.py
@@ -2,7 +2,8 @@ try:
     x = int(input("What's x? "))
     print(f"x is {x}")
 
-except:
+except ValueError:
     print("Please input a number")
 
 # the user might input a string (value error)
+# it's good to be specific with the error instead of leaving "except" as is
