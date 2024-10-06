17391d4626bd2cc10ad62392d84ff9bc6d119299
Ren <rg326@live.com>
2024-10-05 23:52:42 -0400

    added print line to 12_number.py

--- lec3-exceptions/12_number.py
+++ lec3-exceptions/12_number.py
@@ -5,5 +5,6 @@ try:
 except ValueError:
     print("Please input a number")
 
+print(f"x is {x}")
 # the user might input a string (value error)
 # it's good to be specific with the error instead of leaving "except" as is
