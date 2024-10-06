a433ce2a251761d8ea05ad52e46d1482ecf4e25a
Ren <rg326@live.com>
2024-10-05 23:52:42 -0400

    added note to prev print line in 12_number.py

--- lec3-exceptions/12_number.py
+++ lec3-exceptions/12_number.py
@@ -5,6 +5,7 @@ try:
 except ValueError:
     print("Please input a number")
 
+# problem with NameError (due to the order of operations)
 print(f"x is {x}")
 # the user might input a string (value error)
 # it's good to be specific with the error instead of leaving "except" as is
