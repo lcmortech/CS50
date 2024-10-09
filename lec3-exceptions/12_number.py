af0825253498b96641978bfe869e0b48ef9fbc61
Ren <rg326@live.com>
2024-10-08 23:15:52 -0400

    added prev while loop to get_int() and added mai() to 12_number.py

--- lec3-exceptions/12_number.py
+++ lec3-exceptions/12_number.py
@@ -13,17 +13,19 @@ else:
 # it's good to be specific with the error instead of leaving "except" as is
 '''
 
-def get_int():
-    pass
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-while True:
-    try:
-        x = int(input("What's x? "))
-        break # instead of else: break
-    except ValueError:
-        print("x is not an integer")
-    #else:
-        #break
+def get_int():
+	while True:
+    	try:
+        	x = int(input("What's x? "))
+        	break # instead of else: break
+    	except ValueError:
+        	print("x is not an integer")
+ 		#else:
+        	#break
 print(f"x is {x}")
 
 
