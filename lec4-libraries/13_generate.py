5025c4361506e53ed94668b358c0fbb4b92639f5
Lauren <rg326@live.com>
2024-10-12 23:27:30 -0400

    added code for random.shuffle() in 13_generate.py

--- lec4-libraries/13_generate.py
+++ lec4-libraries/13_generate.py
@@ -8,3 +8,11 @@ print(coin)
 #random.randint(a, b)
 number = random.randint(1, 10)
 print(number)
+
+# random.shuffle(x)
+# it shuffles the argument in place
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+
+for card in cards:
+    print(card)
