5b6334e066b1fc49215bca16e20ff0b33a62a8c1
Lauren <rg326@live.com>
2024-10-15 23:53:33 -0400

    added notes about name error bug to 15_name.py

--- lec4-libraries/15_name.py
+++ lec4-libraries/15_name.py
@@ -1,11 +1,13 @@
 import sys
 
 # if you attempt to print a full name
+# Check for errors
 if len(sys.argv) < 2:
     print("Too few arguments")
 elif len(sys.argv) > 2:
 	print("Too many arguments")
-else:
+
+# printing name tags while not checking for arguments
 	print("Hello, my name is", sys.argv[1])
 
 # prints first name given in command line
