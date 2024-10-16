593b6cd50c83539db399f7cfbd21b38da61e3950
Lauren <rg326@live.com>
2024-10-15 23:49:34 -0400

    added conditionals to 15_name.py

--- lec4-libraries/15_name.py
+++ lec4-libraries/15_name.py
@@ -1,6 +1,12 @@
 import sys
 
-print("Hello, my name is", sys.argv[1])
+# if you attempt to print a full name
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+	print("Too many arguments")
+else:
+	print("Hello, my name is", sys.argv[1])
 
 # prints first name given in command line
 # >> "Hello, my name is Name"
