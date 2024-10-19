ed865d25722e7a1e450962184dcc077ff2cce6b2
Lauren <rg326@live.com>
2024-10-18 23:17:41 -0400

    added notes on terminal input to 16_say.py

--- lec4-libraries/16_say.py
+++ lec4-libraries/16_say.py
@@ -8,3 +8,8 @@ import sys
 
 if len(sys.argv) == 2:
 	cowsay.cow("Hello, " + sys.argv[1])
+
+# ++ Terminal Window ++
+# python say.py Name
+# python say.py 
+# python say.py Name >> produces a dinosaur
