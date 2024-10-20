0e195febec5e6c1daf5ab48ef211b26d105aa91d
Ren <rg326@live.com>
2024-10-19 23:33:41 -0400

    added notes on json library to 17_itunes.py

--- lec4-libraries/17_itunes.py
+++ lec4-libraries/17_itunes.py
@@ -11,3 +11,5 @@ if len(sys.argv) != 2:
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
 print(response.json())
 
+# docs.python.org/3/library/json.html
+# json library
