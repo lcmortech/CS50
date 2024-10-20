bb013ec60349d0ff94c276bbbb1ce766da03e8d9
Ren <rg326@live.com>
2024-10-19 23:33:41 -0400

    added the json library to code in 17_itunes.py

--- lec4-libraries/17_itunes.py
+++ lec4-libraries/17_itunes.py
@@ -2,6 +2,7 @@
 # pypi.org/project/requests
 # JSON
 
+import json
 import requests
 import sys
 
@@ -9,7 +10,7 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(response.json())
+print(json.dumps(response.json(), indent=2)
 
 # docs.python.org/3/library/json.html
 # json library
