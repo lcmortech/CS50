5ae9fa2b7c4ec9c35b410c8e35ea013f51d9ce06
Lauren <rg326@live.com>
2024-10-26 23:23:10 -0400

    added assert statements to try-except blocks in 21_test_calculator.py

--- lec5-unittests/21_test_calculator.py
+++ lec5-unittests/21_test_calculator.py
@@ -8,8 +8,15 @@ def test_square():
     #    print("2 squared was not 4")
     #if square(3) != 9:
     #    print("3 squared was not 9")
-    assert square(2) == 4
-    assert square(3) == 9
+
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 squared was not 4")
+    try:
+        assert square(3) == 9
+    except:
+        print("3 squared was not 9")
 if __name__ == "__main__":
     main()
 
