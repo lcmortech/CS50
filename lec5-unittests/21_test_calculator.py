3f9ef7804d2c964a15af6d74a517e1db72160d9b
Lauren <rg326@live.com>
2024-10-27 23:34:44 -0400

    added another try-except block for squaring neg numbers to 21_test_calculator.py

--- lec5-unittests/21_test_calculator.py
+++ lec5-unittests/21_test_calculator.py
@@ -17,6 +17,14 @@ def test_square():
         assert square(3) == 9
     except:
         print("3 squared was not 9")
+	try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 squared was not 4")
+    try:
+        assert square(-3) == 9
+    except:
+        print("-3 squared was not 9")
 if __name__ == "__main__":
     main()
 
