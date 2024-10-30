e9cbcc084ea067d3170347a7aee157b47cba98cd
Lauren <rg326@live.com>
2024-10-29 22:48:22 -0400

    added notes on pytest to 23_testcalcv2.py

--- lec5-unittests/23_testcalcv2.py
+++ lec5-unittests/23_testcalcv2.py
@@ -7,4 +7,7 @@ def test_square():
     assert square(-3) == 9
     assert square(0) == 0
 
-# simply test using pytest
+# simply test using pytest, a third party tool
+# pytest automates the testing process
+# pytest only tests function given in test file, not all the logic in main
+# it's better to break up your ideas into smaller bite-sized functions that are testable, like square(n)
