adf331bac8f5886556d04fc2d3b7f88240b15ff9
Lauren <rg326@live.com>
2024-10-30 23:44:30 -0400

    moved pytest notes from 23_testcalcv2.py

--- lec5-unittests/23_testcalcv2.py
+++ lec5-unittests/23_testcalcv2.py
@@ -20,10 +20,3 @@ def test_negative():
 def test_zero():
 	assert square(0) == 0	
 
-# simply test using pytest, a third party tool
-# pytest automates the testing process
-# pytest only tests function given in test file, not all the logic in main
-# it's better to break up your ideas into smaller bite-sized functions that are testable, like square(n)
-# check50 is a harvard specific alternative to pytest
-# if one test fails it stops other tests from beinig run
-# because of this, it's better to break u one big test into multiple smaller tests by category
