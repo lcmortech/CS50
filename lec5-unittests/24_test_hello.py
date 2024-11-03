75cae4b15b93b865518aa7d67049291c39d8fb9d
Ren <rg326@live.com>
2024-11-02 23:03:35 -0400

    edited initializing code in 24_test_hello.py

--- lec5-unittests/24_test_hello.py
+++ lec5-unittests/24_test_hello.py
@@ -1,4 +1,12 @@
 from hello import hello
 
-def test_hello():
-    hello("Name") == "hello, Name" 
+def main():
+    hello()
+
+#def test_hello():
+#    hello("Name") == "hello, Name" 
+def hello(to="world"):
+    print("hello,",to)
+
+if __name__ == "__main__":
+    main()
