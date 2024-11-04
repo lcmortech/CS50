6d011117b142e7147dc8040bdc79d932888c25ef
Ren <rg326@live.com>
2024-11-03 21:47:05 -0500

    added file 24_hello.py to lec5-unittests

---
+++ lec5-unittests/24_hello.py
@@ -0,0 +1,16 @@
+from hello import hello
+
+def main():
+    hello()
+
+#def test_hello():
+#    hello("Name") == "hello, Name" 
+
+# def hello(to="world"):
+#     print("hello,",to)
+
+# if __name__ == "__main__":
+#     main()
+def main():
+	name. = input("What's your name? ")
+	
