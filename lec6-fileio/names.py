b4b6a3995feb58737c51b9849556a44f18c03657
Lauren <rg326@live.com>
2024-11-09 23:41:44 -0500

    name

--- lec6-fileio/names.py
+++ lec6-fileio/names.py
@@ -1,6 +1,9 @@
 names = []
 
+# for _ in range(3):
+#     name = input("What's your name?" )
+#     names.append[name]
+# # print(f"hello, {name}")
+# v2
 for _ in range(3):
-    name = input("What's your name?" )
-    names.append[name]
-# print(f"hello, {name}")
+	names.append(input("What's your name?" ))
