7fa32e8b23a9f6a7ddc87c8e48fc1c21a914ca81
Lauren <rg326@live.com>
2024-11-10 23:41:56 -0500

    added file creation to names.py

--- lec6-fileio/names.py
+++ lec6-fileio/names.py
@@ -1,13 +1,21 @@
-names = []
+# names = []
 
+# # for _ in range(3):
+# #     name = input("What's your name?" )
+# #     names.append[name]
+# # # print(f"hello, {name}")
+# # v2
 # for _ in range(3):
-#     name = input("What's your name?" )
-#     names.append[name]
-# # print(f"hello, {name}")
-# v2
-for _ in range(3):
-	names.append(input("What's your name?" ))
+# 	names.append(input("What's your name?" ))
 
-for name in sorted(names):
-	print(f"hello", {name})
-# made shorter
+# for name in sorted(names):
+# 	print(f"hello", {name})
+# # made shorter
+
+# OPEN keyword - allows you to specify what you want to read from or write to
+
+names = input("What's your name? ")
+
+file = open("names.txt", "w") # creates file and allows you to write to it
+file.write(name)
+file.close()
