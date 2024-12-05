3c3612179243aaf90ccb7cdd7d14b07d3faddfea
Ren <rg326@live.com>
2024-12-04 23:50:03 -0500

    added writer code to students.py

--- lec6-fileio/students.py
+++ lec6-fileio/students.py
@@ -2,3 +2,7 @@ import csv
 
 name = input("What is your name?")
 home = input("Where is your home?")
+
+with open("students.csv", "a") as file:
+    writer = csv.writer(file)
+    writer.writerow([name, home])
