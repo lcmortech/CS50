584b427bd7785f33eea19b8ab0ac89191c53fc1c
Lauren <rg326@live.com>
2024-12-07 23:43:51 -0500

    added code for dictwriter to students.py

--- lec6-fileio/students.py
+++ lec6-fileio/students.py
@@ -3,8 +3,11 @@ import csv
 name = input("What is your name?")
 home = input("Where is your home?")
 
-with open("students.csv", "a") as file:
-    writer = csv.writer(file)
-    writer.writerow([name, home])
+# with open("students.csv", "a") as file:
+   # writer = csv.writer(file)
+   # writer.writerow([name, home])
 
 # another way to implement this is using a dictionary (DictWriter)
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
