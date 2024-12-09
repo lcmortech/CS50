6e072a68aafa62317f4fdaf5ca351ebc34c3c0d5
Lauren <rg326@live.com>
2024-12-09 18:10:32 -0500

    added url and notes on the pillow library to students.py

--- lec6-fileio/students.py
+++ lec6-fileio/students.py
@@ -11,3 +11,5 @@ home = input("Where is your home?")
 with open("students.csv", "a") as file:
     writer = csv.DictWriter(file, fieldnames=["name", "home"])
     writer.writerow({"name": name, "home": home})
+
+# PIL - pillow.readthedocs.io - allows you to navigate image files as well and to perform operations on image files, and even animate them
